133bf5b5af072084134f79900d362c4ab9f706e3
Mark Grian Tisoy <iamarkgrian@gmail.com>
2021-07-01 06:48:30 +0800

    Update 2

--- app/script.js
+++ app/script.js
@@ -9,3 +9,15 @@ window.onscroll = function () {
         navbar.classList.remove("scroll");
     }
 };
+
+function initMap() {
+    const uluru = { lat: -25.344, lng: 131.036 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+      });
+      const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+      });
+}
