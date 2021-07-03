2cefb26ffe6f712e36ed76cf3ae95bf540ff2eb4
Mark Grian Tisoy <iamarkgrian@gmail.com>
2021-07-03 16:40:29 +0800

    Update 2

--- app/script.js
+++ app/script.js
@@ -1,4 +1,7 @@
 let navbar = document.getElementById("nav");
+let menu = document.querySelector('.mainMenu');
+let openNavbar = document.querySelector('.navbar');
+let showMenu = false;
 
 window.onscroll = function () {
     if(document.body.scrollTop >= 10 || document.documentElement.scrollTop >= 10) {
@@ -10,4 +13,21 @@ window.onscroll = function () {
     }
 };
 
+
+function mainMenuToggle() {
+    if(!showMenu) 
+    {
+        menu.classList.add("activeMenu");
+        openNavbar.style.display = "flex";
+        showMenu = true;
+    }
+    else
+    {
+        menu.classList.remove("activeMenu");
+        openNavbar.style.display = "none";
+        showMenu = false;
+    }
+}
+
+menu.addEventListener('click', mainMenuToggle);
 
